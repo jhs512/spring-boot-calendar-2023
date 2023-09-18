bc5d2e792097944e0ce5298a32e007c29794f0ff
jhs512 <jangka512@gmail.com>
2023-09-18 12:37:15 +0900

    createDate, modifyDate 추가

--- src/main/java/com/sbs/exam1/domain/article/entity/Article.java
+++ src/main/java/com/sbs/exam1/domain/article/entity/Article.java
@@ -1,9 +1,15 @@
 package com.sbs.exam1.domain.article.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -13,10 +19,15 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @AllArgsConstructor
 @Builder
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 public class Article {
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
+    @CreatedDate
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
     private String title;
 }
